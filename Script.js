8e9b291da5b6e91faadcf96f429253510c1fa1b0
tardycornbread <azykh21@protonmail.com>
2023-11-28 18:09:27 -0500

    111

---
+++ Script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    var images = ["img/1-A.png", "img/1-B.png", "img/1-C.png"]; // Add paths to your images
+    var currentImageIdx = 0;
+
+    function updateGalleryImage(index) {
+        $('.gallery-image').attr('src', images[index]);
+        // Update the description based on the image
+        $('.image-description').text("Description for image " + (index + 1));
+    }
+
+    $('.next-button').click(function() {
+        currentImageIdx = (currentImageIdx + 1) % images.length;
+        updateGalleryImage(currentImageIdx);
+    });
+
+    $('.prev-button').click(function() {
+        currentImageIdx = (currentImageIdx - 1 + images.length) % images.length;
+        updateGalleryImage(currentImageIdx);
+    });
+
+    // Initialize with the first image description
+    updateGalleryImage(0);
+});
